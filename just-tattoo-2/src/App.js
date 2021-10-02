028bb6053b9700aefdc59e7117a0263219765497
Watcharin <twatcharin90@gmail.com>
2021-10-02 14:16:25 +0700

    jsx

--- just-tattoo-2/src/App.js
+++ just-tattoo-2/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app" >
+      <header className="app-header">
+        <h4>Just Tattoo</h4>
       </header>
+
+      <div className="app-grid">
+        <div>lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolorlorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor</div>
+        <div>lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolorlorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor</div>
+        <div>lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolorlorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor</div>
+        <div>lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolorlorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor</div>
+      </div>
+
+
     </div>
   );
 }
