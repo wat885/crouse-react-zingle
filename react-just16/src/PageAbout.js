f9964e4c8b9093743ad1d98ab7b1d33aaa0895d9
Watcharin <twatcharin90@gmail.com>
2021-10-08 16:03:03 +0700

    18Nested routes

--- react-just16/src/PageAbout.js
+++ react-just16/src/PageAbout.js
@@ -1,11 +1,33 @@
+import { Link, Route, Switch, Redirect } from "react-router-dom";
 import LayoutPage from "./LayoutPage";
 
 function PageAbout() {
-    return (
-        <LayoutPage>
-            <h2>About เกี่ยวกับเรื่องของหมู่เฮา</h2>
-        </LayoutPage>
-    );
+  return (
+    <LayoutPage>
+      <h2>About เกี่ยวกับเรื่องของหมู่เฮา</h2>
+      <p>
+        <Link to="/about/myself">Myself</Link>
+      </p>
+      <p>
+        <Link to="/about/history">History</Link>
+      </p>
+
+      <Switch>
+        <Route path="/about/myself">
+          <h3>Myself</h3>
+          <p>เรื่องของผม</p>
+        </Route>
+        <Route path="/about/history">
+          <h3>history</h3>
+          <p>อดีตช่างมัน ?</p>
+        </Route>
+
+        <Route path="/about">
+          <Redirect to="/about/myself" />
+        </Route>
+      </Switch>
+    </LayoutPage>
+  );
 }
 
 export default PageAbout;
